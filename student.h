e7528b0cc5fee531d550963d5951683eaea46553
samahazh <samah.azhani1@etu.cyu.fr>
2026-05-21 18:29:37 +0200

    Update student.h

--- student.h
+++ student.h
@@ -1,9 +1,8 @@
-#ifndef STUDENT_H // Vérifie si l'ordinateur n'a pas déjà lu ce fichier "STUDENT_H".
-
-#define STUDENT_H // Si ce n'est pas le cas, on le définit maintenant.
+#ifndef ETUDIANT_H // Vérifie si l'ordinateur n'a pas déjà lu ce fichier ETUDIANT_H
+#define ETUDIANT_H // Si ce n'est pas le cas, on le définit maintenant.
 
 #include "qcm_structure.h" // Contient nos structures
 
-void launchStudentMode(); // Fonction principale qui lance le menu de l'étudiant
+void lancerModeEtudiant(); // Fonction principale qui lance le menu de l'étudiant
 
 #endif
