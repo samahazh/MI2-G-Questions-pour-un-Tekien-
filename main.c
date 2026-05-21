a2a91a547d403c0bfa330a6d16ea17de72f9ef5e
samahazh <samah.azhani1@etu.cyu.fr>
2026-05-21 18:28:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>    // Bibliothèque pour utiliser printf et fgets (affichage et saisie)
 #include <stdlib.h>   // Bibliothèque pour utiliser atoi (convertir du texte en chiffre)
 #include "qcm_structure.h" // Contient nos structures
-#include "teachers.h"      // Permet de faire le lien avec les fonctions du prof
-#include "student.h"       // Permet de faire le lien avec les fonctions de l'étudiant
+#include "enseignant.h"    // Permet de faire le lien avec les fonctions du prof
+#include "etudiant.h"      // Permet de faire le lien avec les fonctions de l'étudiant
 
 /* ================= COULEURS ================= */
-#define COLOR_RESET   "\033[0m"
-#define COLOR_BLUE    "\033[1;34m"
-#define COLOR_RED     "\033[1;31m"
-#define COLOR_GREEN   "\033[1;32m"
-#define COLOR_YELLOW  "\033[1;33m"
-#define COLOR_PINK   "\033[1;35m"
+#define COULEUR_DEFAUT "\033[0m"
+#define COULEUR_BLEU   "\033[1;34m"
+#define COULEUR_ROUGE  "\033[1;31m"
+#define COULEUR_VERT   "\033[1;32m"
+#define COULEUR_JAUNE  "\033[1;33m"
+#define COULEUR_ROSE   "\033[1;35m"
 /* ================================================= */
 
 int main() {
@@ -20,13 +20,13 @@ int main() {
     // On utilise une boucle "do... while" : le menu s'affiche au moins une fois, et s'affiche tant que l'utilisateur ne tape pas 3.
     do {
         // --- AFFICHAGE DU MENU PRINCIPAL ---
-        printf(COLOR_BLUE "\n==========================================\n" COLOR_RESET);
-        printf(COLOR_BLUE "          QUESTIONS POUR UN TEKIEN\n" COLOR_RESET); 
-        printf(COLOR_BLUE "==========================================\n" COLOR_RESET);
+        printf(COULEUR_BLEU "\n==========================================\n" COULEUR_DEFAUT);
+        printf(COULEUR_BLEU "          QUESTIONS POUR UN TEKIEN\n" COULEUR_DEFAUT); 
+        printf(COULEUR_BLEU "==========================================\n" COULEUR_DEFAUT);
         
-        printf(COLOR_RED   " 1 - Mode Enseignant (Creer un QCM)\n" COLOR_RESET);
-        printf(COLOR_GREEN " 2 - Mode Etudiant (Passer un QCM)\n" COLOR_RESET);
-        printf(COLOR_PINK" 3 - Quitter l'application\n" COLOR_RESET);
+        printf(COULEUR_ROUGE " 1 - Mode Enseignant (Creer un QCM)\n" COULEUR_DEFAUT);
+        printf(COULEUR_VERT  " 2 - Mode Etudiant (Passer un QCM)\n" COULEUR_DEFAUT);
+        printf(COULEUR_ROSE  " 3 - Quitter l'application\n" COULEUR_DEFAUT);
         
         printf("------------------------------------------\n");
         printf("Veuillez entrer votre choix (1 a 3) : ");
@@ -44,12 +44,12 @@ int main() {
         switch (choix) {
             case 1:
                 printf("\n[Lancement du Mode Enseignant]\n");
-                launchTeacherMode(); // On appelle la fonction du fichier teachers.c
+                lancerModeEnseignant(); 
                 break; // On sort du switch
 
             case 2:
                 printf("\n[Lancement du Mode Etudiant]\n");
-                launchStudentMode(); // On appelle la fonction du fichier student.c
+                lancerModeEtudiant(); 
                 break;
 
             case 3:
@@ -59,8 +59,8 @@ int main() {
 
             default:
                 // Si l'utilisateur tape 4, 0 ou des lettres (atoi renvoie 0), on affiche une erreur
-                printf(COLOR_RED "\n--- Erreur : Saisie invalide ! ---\n");
-                printf("--- Veuillez entrer un chiffre entre 1 et 3. ---\n\n" COLOR_RESET);
+                printf(COULEUR_ROUGE "\n--- Erreur : Saisie invalide ! ---\n");
+                printf("--- Veuillez entrer un chiffre entre 1 et 3. ---\n\n" COULEUR_DEFAUT);
                 break;
         }
 
