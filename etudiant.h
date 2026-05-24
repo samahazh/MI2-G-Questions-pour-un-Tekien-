f6052a4d6528459ec448252aca8f7dbcd033a324
samahazh <samah.azhani1@etu.cyu.fr>
2026-05-24 21:26:50 +0200

    Update etudiant.h

--- etudiant.h
+++ etudiant.h
@@ -1,10 +1,10 @@
-#ifndef ETUDIANT_H // Vérifie si l'ordinateur n'a pas déjà lu ce fichier ETUDIANT_H
+#ifndef ETUDIANT_H // Vérifie si l'ordinateur n'a pas déjà lu le fichier ETUDIANT_H.
 #define ETUDIANT_H // Si ce n'est pas le cas, on le définit maintenant.
 
-#include "qcm_structure.h" // Contient nos structures
+#include "qcm_structure.h" // Contient nos structures et les couleurs.
 
 /*
- * Fonction principale du mode etudiant.
+ * Fonction principale du mode étudiant.
  * Appelée depuis le menu principal pour gérer le passage du QCM et la notation.
  */
 void lancerModeEtudiant(); 
